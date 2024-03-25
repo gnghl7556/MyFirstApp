fc48e1b2efc943f277b169aecb465d58ce70b59f
gnghl7556 <gnghl7556@naver.com>
2024-03-25 17:05:54 +0900

    기초강의 완료, 기초 레이아웃 설정방법과 오토레이아웃, IBAction과 IBOutlet에 대한 기초학습

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBOutlet weak var myButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.view.backgroundColor = #colorLiteral(red: 0.9568627477, green: 0.6588235497, blue: 0.5450980663, alpha: 1)
     }
 
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        mainLabel.text = "안녕하세요"
+        mainLabel.textColor = UIColor.white
+        mainLabel.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        
+        
+        myButton.setTitleColor(UIColor.black, for: UIControl.State.normal)
+        myButton.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+    }
+    
 
 }
 
